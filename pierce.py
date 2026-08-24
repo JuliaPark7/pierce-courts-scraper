9cea8690b0d30d58b0271f6c42f18f241fcbd027
Julia Park <parkjul7@uw.edu>
2026-08-24 14:43:03 -0700

    added script to auto-delete old roster files

--- pierce.py
+++ pierce.py
@@ -12,7 +12,7 @@ import os
 
 # landing page for the jail roster
 URL = "https://linxonline.co.pierce.wa.us/linxweb/Booking/GetJailRoster.cfm"
-data_dir = Path("rosters1")
+data_dir = Path("rosters")
 
 # a path to a folder called rosters. if it doesnt exist create it 
 data_dir.mkdir(exist_ok=True)
@@ -21,24 +21,6 @@ data_dir.mkdir(exist_ok=True)
 # def scrape_detail_page(html):
   #  """Parse the HTML of an inmate detail page"""
    # pass
-
-"""
-
-def scrape_charges(charges)
-
-    # NOW, grab the table with the charges in it 
-    charges = soup.find_all("table", {"cols": "9"})
-    if charges is None:
-        print(f"Warning: no charges found in file")
-        continue
-    charges_rows = charges.find_all("tr")
-        for charge_row in charges_rows:
-        charge_cells = charge_row.find_all("td")
-        charge_name = charge_cells[1].text
-        print(charge_name)
-
-"""
-
 
 def scrape_inmate_details():
     data = []
@@ -50,7 +32,11 @@ def scrape_inmate_details():
                 html_content = file.read()
                 soup = BeautifulSoup(html_content, 'html.parser')
 
-                bookingID = soup.find('h1').text
+                bookingID = soup.find('h1')
+                if bookingID: 
+                    bookingID_text = bookingID.text
+                else:
+                    bookingID = None
                 # print(bookingID)
 
                 # grab the table with the inmate's details in it 
@@ -78,7 +64,7 @@ def scrape_inmate_details():
 
                 # create a dictionary to store the inmate's details
                 inmate_dict = {
-                    "bookingID": bookingID,
+                    "bookingID": bookingID_text,
                     "inmate_name": inmate_name,
                     "booking_time": booking_time,
                     "arresting agency": agency
@@ -146,53 +132,16 @@ def scrape_inmate_details():
             data.append(inmate_dict)
             df = pd.DataFrame(data)
             df.to_csv('output.csv', index=False)
+
     print("CSV done!")
+    for roster in data_dir.iterdir():
+            if roster.is_file():
+                roster.unlink()
+    print("all files deleted successfully!")
     
                         
-               
-    """ 
-                scraped_details = {
-                    "bookingID": bookingID,
-                    "inmate_name": inmate_name,
-                    "booking_time": booking_time,
-                    "agency": agency,
-                    "charge": charge,
-                    "charging_agency": charging_agency,
-                    "jurisdiction": jurisdiction,
-                    "cause_number": cause_number,
-                    "full_url": full_url
-                      }
-        """
-                    
-                
 
-                # turn that info into a DataFrame      
-
-"""" 
-                    if len(charge_data) > 0:
-                        charge = charge_data[1].text
-                        print(charge)
-                    else:
-                        charge = "No text found"
-                        
-"""
-                    # charge = charge_data[1].text
-                    # print(f"Charge {index}: {charge}")
-
-"""
-                charge_rows = charges.find_all("tr")[1]
-                charge_data = charge_row.find_all("td")
-                charge = charge_data[1].text
-                """
-
-                # grab the row with the jurisdiction in it 
-              
-
-if __name__ == "__main__":
-    scrape_inmate_details()
-
-        
-                  
+   
         # grab booking ID, inmate name, booking date/time, 
         # charges, warrant type, charging agency, 
         # court case number, court date, booking ID, court date,
@@ -208,49 +157,102 @@ if __name__ == "__main__":
 
         # have this function run every day 
 
-
-#def scrape_detail_page(html)
-
 """ 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto(URL)
-    expect(page.get_by_text("Booked Less Than 24 Hours Ago")).to_be_visible()
-    html = page.inner_html("body")
-    soup = BeautifulSoup(html, "html.parser")
-    table = soup.find_all("table")[-1]
 
-    for row in table.find_all("tr")[1:]:
-        cells = row.find_all("td")
-        name = cells[1].text
-        booking_id = cells[2]
-        detail_url = urljoin(
-            URL,
-            booking_id.find("a").get("href")
-        )
-        booking_id = booking_id.text.strip()
-        location = cells[3].text
-        release_date = cells[4].text.strip()
-        if release_date:
-            release_date = datetime.strptime(
-                cells[4].text, "%m/%d/%Y %I:%M %p"
-            ).isoformat()
-        # https://linxonline.co.pierce.wa.us/linxweb/Booking/GetBooking.cfm?booking_id=2026209028
-        print(name, booking_id, detail_url, location, release_date)
-        filepath = data_dir / f"{booking_id}.html"
-        if not filepath.exists():
-            page.goto(detail_url)
+def scrape_asterisks():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(URL)
+        expect(page.get_by_text("Booked Less Than 72 Hours Ago")).to_be_visible()
+        html = page.inner_html("body")
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find_all("table")[-1]
 
-            time.sleep(2)
+        for row in table.find_all("tr")[1:]:
+            cells = row.find_all("td")
+            booked_24hrs = cells[0].text
+            if "*" in booked_24hrs:
+                name = cells[1].text
+                booking_id = cells[2]
+                detail_url = urljoin(
+                URL,
+                booking_id.find("a").get("href")
+                )
+                booking_id = booking_id.text.strip()
+                location = cells[3].text
+                release_date = cells[4].text.strip()
+                if release_date:
+                    release_date = datetime.strptime(
+                        cells[4].text, "%m/%d/%Y %I:%M %p"
+                    ).isoformat()
+            # https://linxonline.co.pierce.wa.us/linxweb/Booking/GetBooking.cfm?booking_id=2026209028
+                print(name, booking_id, detail_url, location, release_date)
+                filepath = data_dir / f"{booking_id}.html"
+                if not filepath.exists():
+                    page.goto(detail_url)
 
-            # w means opening the file in write mode. 
-            with open(filepath,"w") as out_file: 
-                out_file.write(page.inner_html("body"))
-            print(f"downloaded {filepath}")
-            time.sleep(3)
-            page.go_back()
+                    time.sleep(2)
 
-    browser.close()
+                # w means opening the file in write mode. 
+                with open(filepath,"w") as out_file: 
+                    out_file.write(page.inner_html("body"))
+                print(f"downloaded {filepath}")
+                time.sleep(3)
+                page.go_back()
+
+        browser.close()
+
 
 """
+""" 
+
+def scrape_page():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(URL)
+        expect(page.get_by_text("Booked Less Than 72 Hours Ago")).to_be_visible()
+        html = page.inner_html("body")
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find_all("table")[-1]
+
+        for row in table.find_all("tr")[1:]:
+            cells = row.find_all("td")
+            name = cells[1].text
+            booking_id = cells[2]
+            detail_url = urljoin(
+                URL,
+                booking_id.find("a").get("href")
+            )
+            booking_id = booking_id.text.strip()
+            location = cells[3].text
+            release_date = cells[4].text.strip()
+            if release_date:
+                release_date = datetime.strptime(
+                    cells[4].text, "%m/%d/%Y %I:%M %p"
+                ).isoformat()
+            # https://linxonline.co.pierce.wa.us/linxweb/Booking/GetBooking.cfm?booking_id=2026209028
+            print(name, booking_id, detail_url, location, release_date)
+            filepath = data_dir / f"{booking_id}.html"
+            if not filepath.exists():
+                page.goto(detail_url)
+
+                time.sleep(2)
+
+                # w means opening the file in write mode. 
+                with open(filepath,"w") as out_file: 
+                    out_file.write(page.inner_html("body"))
+                print(f"downloaded {filepath}")
+                time.sleep(3)
+                page.go_back()
+
+        print("html files downloaded!")
+
+        browser.close()
+
+"""
+
+if __name__ == "__main__":
+    # scrape_asterisks()
+    scrape_inmate_details()
